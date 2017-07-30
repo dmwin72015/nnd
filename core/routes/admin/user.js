e2d8fb941066e180cc5c12621796714bb3305601
dongmin <marourou2016@icloud.com>
2017-07-30 20:04:31 +0800

    user update

--- core/routes/admin/user.js
+++ core/routes/admin/user.js
@@ -43,7 +43,7 @@ let reqPostHander = {
         });
     },
     update: function(req, res, next) {
-
+        
 
 
     }
@@ -52,43 +52,43 @@ let reqPostHander = {
 module.exports = {
     '/': function(req, res, next) {
         var curUser = req.session.loginInfo;
-        // if (curUser) {
-        userMod.findOne({ _id: "595722c7f015e715fc902598" }).lean().exec(function(err, data) {
-            if (err || !data) {
-                next();
-                return;
-            }
-            var tmpl_data = {
-                uid: data.uid,
-                name: data.uname || data.uid,
-                age: data.age || '',
-                sex: data.sex || '',
-                nick_name: data.alias || data.uid,
-                group: data.gname || '无',
-                reg_date: data.created.toLocaleString()
-            };
-            var re_data = {};
-            var _sex = '';
-            if (data.sex == 1) {
-                _sex = "男"
-            } else if (data.sex == 0) {
-                _sex = "女"
-            }
-            var userData = [
-                { title: 'ID', val: data.uid },
-                { title: '姓名', val: data.uname || data.uid },
-                { title: '年龄', val: data.age || '' },
-                { title: '性别', val: _sex },
-                { title: '昵称', val: data.alias || data.uid },
-                { title: '组', val: data.gname || '无' },
-                { title: '注册时间', val: data.created.toLocaleString() }
-            ];
-            re_data.userData = JSON.stringify(userData);
-            res.render('user/index.html', re_data);
-        });
-        // } else {
-        //     res.redirect('/admin/login');
-        // }
+        if (curUser) {
+            userMod.findOne({ _id: "595722c7f015e715fc902598" }).lean().exec(function(err, data) {
+                if (err || !data) {
+                    next();
+                    return;
+                }
+                var tmpl_data = {
+                    uid: data.uid,
+                    uname: data.uname || data.uid,
+                    age: data.age || '',
+                    sex: data.sex || '',
+                    nick_name: data.alias || data.uid,
+                    group: data.gname || '无',
+                    reg_date: data.created.toLocaleString()
+                };
+                var re_data = {};
+                var _sex = '';
+                if (data.sex == 1) {
+                    _sex = "男"
+                } else if (data.sex == 0) {
+                    _sex = "女"
+                }
+                var userData = {
+                    uid: { title: 'ID', val: data.uid, edit: 0 },
+                    uname: { title: '姓名', val: data.uname || data.uid, edit: 1 },
+                    age: { title: '年龄', val: data.age || '', edit: 1 },
+                    sex: { title: '性别', val: _sex, edit: 1 },
+                    nick_name: { title: '昵称', val: data.alias || data.uid, edit: 1 },
+                    group: { title: '组', val: data.gname || '无', edit: 0 },
+                    reg_date: { title: '注册时间', val: data.created.toLocaleString(), edit: 0 }
+                };
+                re_data.userData = JSON.stringify(userData);
+                res.render('user/index.html', re_data);
+            });
+        } else {
+            res.redirect('/admin/login');
+        }
     },
 
     '/:action': {
